5b295f1254d2ea385d5c4bc04529e124956393bb
Brian Norman <brian@bnorm.com>
2018-09-29 21:56:58 -0500

    Add Apache 2.0 license to project

--- src/test/kotlin/com/bnorm/ktor/retrofit/RetrofitServiceTest.kt
+++ src/test/kotlin/com/bnorm/ktor/retrofit/RetrofitServiceTest.kt
@@ -1,5 +1,22 @@
+/*
+ * Copyright (C) 2018 Brian Norman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.bnorm.ktor.retrofit
 
+import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.HttpMethod
@@ -21,31 +38,32 @@ interface Service {
   fun getSingle(@Path("id") id: Long): Call<String>
 }
 
+fun Application.module() {
+  install(ContentNegotiation) {
+    jackson { }
+  }
+
+  install(RetrofitService) {
+    service(baseUrl = "api", service = object : Service {
+      override fun getAll(): Call<List<String>> = call {
+        return@call listOf("first", "second")
+      }
+
+      override fun getSingle(id: Long): Call<String> = call {
+        return@call when (id) {
+          0L -> "first"
+          1L -> "second"
+          else -> throw IndexOutOfBoundsException("id=$id")
+        }
+      }
+    })
+  }
+}
+
 class RetrofitServiceTest {
 
   @Test
-  fun feature(): Unit = withTestApplication {
-    //    application.install(DataConversion)
-    application.install(ContentNegotiation) {
-      jackson { }
-    }
-
-    application.install(RetrofitService) {
-      service(baseUrl = "api", service = object : Service {
-        override fun getAll(): Call<List<String>> = call {
-          return@call listOf("first", "second")
-        }
-
-        override fun getSingle(id: Long): Call<String> = call {
-          return@call when (id) {
-            0L -> "first"
-            1L -> "second"
-            else -> throw IndexOutOfBoundsException("id=$id")
-          }
-        }
-      })
-    }
-
+  fun feature(): Unit = withTestApplication(Application::module) {
     with(handleRequest(HttpMethod.Get, "/api/string")) {
       assertEquals(HttpStatusCode.OK, response.status())
       assertEquals("[\"first\",\"second\"]", response.content)
