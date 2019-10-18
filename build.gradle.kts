62077b00eb0060d9b6764f90c1b3a0fa1d16626d
Brian Norman <brian@bnorm.com>
2019-10-18 11:41:21 -0500

    Configure releasing to Sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,26 @@ buildscript {
   }
   dependencies {
     classpath(kotlin("gradle-plugin", version = "1.3.50"))
+    classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.0")
   }
 }
 
-group = "com.bnorm.ktor.retrofit"
-version = "0.1-SNAPSHOT"
+plugins {
+  id("org.jetbrains.dokka") version "0.10.0" apply false
+  id("nebula.release") version "13.1.1"
+}
+
+val release = tasks.findByPath(":release")
+release?.finalizedBy(project.getTasksByName("publish", true))
+
+allprojects {
+  group = "com.bnorm.ktor.retrofit"
+}
+
+subprojects {
+  repositories {
+    mavenCentral()
+    jcenter()
+    maven { setUrl("https://dl.bintray.com/kotlin/ktor") }
+  }
+}
--- ktor-retrofit/build.gradle.kts
+++ ktor-retrofit/build.gradle.kts
@@ -2,11 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm")
-}
+  id("org.jetbrains.dokka")
 
-repositories {
-  jcenter()
-  maven { setUrl("https://dl.bintray.com/kotlin/ktor") }
+  signing
+  `maven-publish`
 }
 
 dependencies {
@@ -22,4 +21,90 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
   kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.dokka {
+  outputFormat = "html"
+  outputDirectory = "$buildDir/javadoc"
+}
+
+tasks.register("sourcesJar", Jar::class) {
+  group = "build"
+  description = "Assembles Kotlin sources"
+
+  archiveClassifier.set("sources")
+  from(sourceSets.main.get().allSource)
+  dependsOn(tasks.classes)
+}
+
+tasks.register("dokkaJar", Jar::class) {
+  group = "documentation"
+  description = "Assembles Kotlin docs with Dokka"
+
+  archiveClassifier.set("javadoc")
+  from(tasks.dokka)
+  dependsOn(tasks.dokka)
+}
+
+signing {
+  setRequired(provider { gradle.taskGraph.hasTask("release") })
+  sign(publishing.publications)
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("default") {
+      from(components["java"])
+      artifact(tasks["sourcesJar"])
+      artifact(tasks["dokkaJar"])
+
+      pom {
+        name.set(project.name)
+        description.set("Turns a Retrofit service interface into Ktor routing")
+        url.set("https://github.com/bnorm/ktor-retrofit")
+
+        licenses {
+          license {
+            name.set("Apache License 2.0")
+            url.set("https://github.com/bnorm/ktor-retrofit/blob/master/LICENSE.txt")
+          }
+        }
+        scm {
+          url.set("https://github.com/bnorm/ktor-retrofit")
+          connection.set("scm:git:git://github.com/bnorm/ktor-retrofit.git")
+        }
+        developers {
+          developer {
+            name.set("Brian Norman")
+            url.set("https://github.com/bnorm")
+          }
+        }
+      }
+    }
+  }
+
+  repositories {
+    if (
+      hasProperty("sonatypeUsername") &&
+      hasProperty("sonatypePassword") &&
+      hasProperty("sonatypeSnapshotUrl") &&
+      hasProperty("sonatypeReleaseUrl")
+    ) {
+      maven {
+        val url = when {
+          "SNAPSHOT" in version.toString() -> property("sonatypeSnapshotUrl")
+          else -> property("sonatypeReleaseUrl")
+        } as String
+        setUrl(url)
+        credentials {
+          username = property("sonatypeUsername") as String
+          password = property("sonatypePassword") as String
+        }
+      }
+    }
+    maven {
+      name = "test"
+      setUrl("file://${rootProject.buildDir}/localMaven")
+    }
+  }
 }
