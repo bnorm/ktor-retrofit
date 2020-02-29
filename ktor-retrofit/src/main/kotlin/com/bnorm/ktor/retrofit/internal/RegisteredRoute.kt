8b8b86d70454d7172e577b88d87b9eb4ca207255
Josh Feinberg <josh@joshafeinberg.com>
2020-02-29 15:49:21 -0600

    Make class internal

--- ktor-retrofit/src/main/kotlin/com/bnorm/ktor/retrofit/internal/RegisteredRoute.kt
+++ ktor-retrofit/src/main/kotlin/com/bnorm/ktor/retrofit/internal/RegisteredRoute.kt
@@ -2,14 +2,13 @@ package com.bnorm.ktor.retrofit.internal
 
 import io.ktor.http.HttpMethod
 
-data class RegisteredRoute private constructor(
+internal data class RegisteredRoute private constructor(
     val method: HttpMethod,
     val baseUrl: String?,
     val route: String
 ) {
     // not included in constructor for equals/hashcode
-    var methodName: String = ""
-        private set
+    private var methodName: String = ""
 
     constructor(
         method: HttpMethod,
